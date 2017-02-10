0c05163614f8edfdf08d0ad17ce5cee12388289f
ken <ken@yappa.be>
2017-02-10 12:36:21 +0100

    Changed setup

--- README.md
+++ README.md
@@ -1,35 +1,30 @@
-# Dash & D
-A dashboard for a dungeon master
+# D&D Stats
 
-## Todo
-- Change quests via (modal)
-- Generate names
-- Generate towns
-- Generate taverns
-- Generate NPC's
-- Generate Treasure
-- Add small sound options
-- Add environment options for sounds
-- Make location modal searchable
-- Pick a location and have it set as primary
-- Show goals for primary options
-- Quests need a step and completion - important when making quests
-- Add more town/city info
-- Add a edit players page/tab that edits the JSON file directly
-- Add start, pause and stop session buttons
-- Personal NPC's need to be added to the list of NPC's
-- Make map scrollable (and clickable) - default Mac is fine for now
+## About
+Webdesigner, webdeveloper, teacher, gamer, moron, realist, procrastinator, fast talker, nerd, distracted by pretty colors. I think thats by far the best description I can give you.
 
-## Done
-- Make it possible to load everything - JSON
-- Add content for stuff - weapons, armor and potions
-- Make it possible to save everything - JSON
-- Time needs to be 20:05, not 20:5
-- Add sorting with initiative (combat)
-- Remove any overflow of body element
-- Add the option to add bad guys in combat sequence
-- Make it possible to take notes per session
-- Add overviews per session via the notes
-- Add content for NPC
-- Story needs better overviews of previous sessions
-- Add icons to town selection
+## For more information
+- [Source and writer](http://kenvandamme.be/) or [on Github as solo244](https://github.com/solo244)
+
+## Requirements/used
+- jQuery
+- SASS for `css`
+- Jade for `html`
+- Grunt
+- Bower
+
+## Browser compatibility
+- Internet Explorer 9+
+- Firefox
+- Chrome
+- Opera
+- Safari
+
+## Set up
+1. Clone files to a new directory
+2. `cd` to the this directory via command line/terminal
+3. Run `npm install` to get your node_modules (default **.gitignore**)
+4. Run `npm install grunt-postcss pixrem autoprefixer-core cssnano`
+5. Run `bower install`
+5. Check `Gruntfile.js` and look at **//Change** comments (settings at the top)
+6. Run `grunt` to start developing
--- dev/js/_main.js
+++ dev/js/_main.js
@@ -1,44 +1,7 @@
 /*
  * Vars
  */
-var $body = $("html, body"),
-    $left = $body.find(".left"), // Left
-    $right = $body.find(".right"), // Right
-    $players = $right.find(".players"),
-    $add_enemy = $right.find(".add-enemy"),
-    $left_location = $left.find(".left_location"),
-    $current_location = $left_location.find(".current_location"),
-    $loc_info = $left_location.find(".info"),
-    $loc_overview = $left_location.find(".overview"),
-    $loc_info_c = $left_location.find(".info_content"),
-    $loc_overview_c = $left_location.find(".overview_content"),
-    $change_location = $current_location.find(".change"),
-    $pages = $body.find(".page"), // Pages
-    $active_page = $body.find(".active_page"),
-    $menu = $body.find(".mainMenu"), // Menu
-    $menu_items = $menu.find("li"),
-    $clock = $body.find(".clock"), // Clock
-    $dices = $right.find(".dices"), // Dices
-    $dice = $right.find(".dice"),
-    $die = $dices.find(".die"),
-    $result = $dice.find(".result"),
-    $combat_btn = $dices.find(".active_combat"),
-    $combat = $right.find(".combat"), // Combat
-    $sort = $right.find(".sort"),
-    $stats = $right.find(".stats"), // Stats
-    $money = $right.find(".money"), // Money
-    $modal_all = $body.find(".modal-overlay"), // Modal
-    $modal = $modal_all.find(".modal"),
-    $modal_close = $modal_all.find(".modal-bg"),
-    $add = $stats.find(".add"),
-    $substract = $stats.find(".substract"),
-    $stuff_submenu = $body.find(".stuff .stuff-menu_item"),
-    $stuff_content = $body.find(".stuff-content"),
-    $npc_submenu = $body.find(".npc .sub-menu_stuff .stuff-menu_item"),
-    $npc_content = $body.find(".npc-content"),
-    $ui_saving = $body.find(".top-saving"),
-    $ui_saved = $body.find(".top-saved");
-
+var $body = $("html, body");
   var player1,
       player2,
       player3,
@@ -51,24 +14,4 @@ var $body = $("html, body"),
  */
 $(document).ready(function(){
   load();
-
-  $ui_saving.hide();
-  $ui_saved.hide();
-
-  $menu_items.on("click", toggleMenu);
-  $die.on("click", rollDie);
-  $combat_btn.on("click", toggleCombat);
-  $modal_close.on("click", closeModal);
-  $change_location.on("click", changeLocation);
-  $add.on("click", addNumber);
-  $substract.on("click", addNumber);
-  $stuff_submenu.on("click", gotoSub);
-  $npc_submenu.on("click", gotoNpc);
-  $sort.on("click", sorting);
-  $add_enemy.on("click", addEnemy);
-  $loc_info.on("click", showLocationInfo);
-  $loc_overview.on("click", showLocationOverview);
-
-  showTime();
-  setBackgrounds();
 });
